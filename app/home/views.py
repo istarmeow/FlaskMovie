d216245ce27dcd733b20689ca906f9e63666470f
xyliurui <1328161560@qq.com>
2018-10-16 21:03:13 +0800

    创建会员中心登录注册

--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,47 @@
 from . import home
+from flask import render_template, redirect, url_for
 
 
 @home.route("/")
 def index():
-    return "<h1 style='color:blue'>前台</h1>"
+    return render_template('home/index.html')
+
+
+@home.route('/login/')
+def login():
+    return render_template('home/login.html')
+
+
+@home.route('/logout/')
+def logout():
+    return redirect(url_for('home.login'))
+
+
+@home.route('/register/')
+def register():
+    return render_template('home/register.html')
+
+
+@home.route('/user/')
+def user():
+    return render_template('home/user.html')
+
+
+@home.route('/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+
+@home.route('/userlog/')
+def userlog():
+    return render_template('home/userlog.html')
+
+
+@home.route('/moviecollect/')
+def moviecollect():
+    return render_template('home/moviecollect.html')
