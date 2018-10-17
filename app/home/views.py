f369c461c0334c2de9f756a80fbee901ca8a22f6
xyliurui <1328161560@qq.com>
2018-10-17 12:08:03 +0800

    完成前端模板

--- app/home/views.py
+++ app/home/views.py
@@ -7,6 +7,11 @@ def index():
     return render_template('home/index.html')
 
 
+@home.route("/indexbanner/")
+def indexbanner():
+    return render_template('home/indexbanner.html')
+
+
 @home.route('/login/')
 def login():
     return render_template('home/login.html')
@@ -45,3 +50,13 @@ def userlog():
 @home.route('/moviecollect/')
 def moviecollect():
     return render_template('home/moviecollect.html')
+
+
+@home.route('/search/')
+def search():
+    return render_template('home/search.html')
+
+
+@home.route('/play/')
+def play():
+    return render_template('home/play.html')
