2ad78fbe7119f2fc311e00bf86f580e77dd08439
xyliurui <1328161560@qq.com>
2018-10-17 18:29:28 +0800

    完成前后端模板搭建

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,6 +1,117 @@
 from . import admin
+from flask import render_template, redirect, url_for
 
 
 @admin.route("/")
 def index():
-    return "<h1 style='color:red'>后台</h1>"
+    return render_template('admin/index.html')
+
+
+@admin.route("/login/")
+def login():
+    return render_template('admin/login.html')
+
+
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
+
+
+@admin.route("/pwd/")
+def pwd():
+    return render_template('admin/pwd.html')
+
+
+@admin.route("/tag_add/")
+def tag_add():
+    return render_template('admin/tag_add.html')
+
+
+@admin.route("/tag_list/")
+def tag_list():
+    return render_template('admin/tag_list.html')
+
+
+@admin.route("/movie_add/")
+def movie_add():
+    return render_template('admin/movie_add.html')
+
+
+@admin.route("/movie_list/")
+def movie_list():
+    return render_template('admin/movie_list.html')
+
+
+@admin.route("/preview_add/")
+def preview_add():
+    return render_template('admin/preview_add.html')
+
+
+@admin.route("/preview_list/")
+def preview_list():
+    return render_template('admin/preview_list.html')
+
+
+@admin.route("/user_list/")
+def user_list():
+    return render_template('admin/user_list.html')
+
+
+@admin.route("/user_view/")
+def user_view():
+    return render_template('admin/user_view.html')
+
+
+@admin.route("/comment_list/")
+def comment_list():
+    return render_template('admin/comment_list.html')
+
+
+@admin.route("/collect_list/")
+def collect_list():
+    return render_template('admin/collect_list.html')
+
+
+@admin.route("/logs/operate_log/")
+def logs_operate_log():
+    return render_template('admin/logs_operate_log.html')
+
+
+@admin.route("/logs/admin_log/")
+def logs_admin_log():
+    return render_template('admin/logs_admin_log.html')
+
+
+@admin.route("/logs/user_log/")
+def logs_user_log():
+    return render_template('admin/logs_user_log.html')
+
+
+@admin.route("/auth_add/")
+def auth_add():
+    return render_template('admin/auth_add.html')
+
+
+@admin.route("/auth_list/")
+def auth_list():
+    return render_template('admin/auth_list.html')
+
+
+@admin.route("/role_add/")
+def role_add():
+    return render_template('admin/role_add.html')
+
+
+@admin.route("/role_list/")
+def role_list():
+    return render_template('admin/role_list.html')
+
+
+@admin.route("/admin_add/")
+def admin_add():
+    return render_template('admin/admin_add.html')
+
+
+@admin.route("/admin_list/")
+def admin_list():
+    return render_template('admin/admin_list.html')
